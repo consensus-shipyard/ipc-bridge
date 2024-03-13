14d933c129fb12594e4228576df6cf72069cbe36
Mikers <snissn@gmail.com>
2024-03-12 22:04:05 -1000

    update info

--- src/utils/site.ts
+++ src/utils/site.ts
@@ -1,6 +1,6 @@
 export const SITE_NAME = 'IPC Bridge'
 export const SITE_DESCRIPTION = 'Quickly bridge tokens'
-export const SITE_URL = 'https://nexth.vercel.app'
+export const SITE_URL = 'https://spiffy-salamander-95199d.netlify.app/'
 
-export const SOCIAL_TWITTER = 'wslyvh'
-export const SOCIAL_GITHUB = 'snissn/nexth'
+export const SOCIAL_TWITTER = 'ipcdevs'
+export const SOCIAL_GITHUB = 'consensus-shipyard/ipc-bridge'
